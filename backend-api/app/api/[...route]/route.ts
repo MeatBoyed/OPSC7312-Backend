d450270b065d292c982c208cc5219679343f6cb5
MeatBoyed <charlierossouw@outlook.com>
2024-08-19 13:11:44 +0200

    feat: Home route has response

--- backend-api/app/api/[...route]/route.ts
+++ backend-api/app/api/[...route]/route.ts
@@ -6,6 +6,9 @@ export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
+app.get("/", (c) => {
+  return c.json({ message: "Hello from OPSC Database API!", secretNumber: Math.floor(Math.random() * 101) });
+});
 app.route("/news", newsController);
 
 export const GET = handle(app);
